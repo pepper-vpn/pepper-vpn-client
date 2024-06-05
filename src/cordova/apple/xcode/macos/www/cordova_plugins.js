61b50a228aafa2b4928394697b65cfb5a5fbd1f7
Alexander Kotelnikov <sacha@myxomop.com>
2024-06-05 14:44:48 +0200

    CDV Device integration

--- src/cordova/apple/xcode/macos/www/cordova_plugins.js
+++ src/cordova/apple/xcode/macos/www/cordova_plugins.js
@@ -15,11 +15,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "WebIntent"
       ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-outline": "0.0.0",
     "cordova-plugin-clipboard": "2.0.0",
-    "cordova-webintent": "2.0.0"
+    "cordova-webintent": "2.0.0",
+    "cordova-plugin-device": "2.1.0.1"
   };
 });
